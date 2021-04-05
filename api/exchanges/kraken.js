cfd0d222aa428614d606f56aff9f9570de24c997
isEvrythngTkn <paulngoertzen@gmail.com>
2021-04-05 09:00:21 -0700

    Adds kraken

--- api/exchanges/kraken.js
+++ api/exchanges/kraken.js
@@ -1,12 +1,19 @@
 const KrakenClient = require('kraken-api');
 const { apiKey, apiSecret } = require('../../config/env/local').kraken;
 
+// there needs to be one api or module for getting all the data formatted so that we can work with it.
+// That module interacts with all the apis and returns consistently formatted objects that we understand
+// Once we have consistent data, we can try to calculate taxes
+
 module.exports = {
   async getTransactions() {
     const kraken = new KrakenClient(apiKey, apiSecret);
 
-    const response = await kraken.api('ClosedOrders');
-    console.log(response.result.closed);
+    const response = await kraken.api('TradesHistory', { ofs: 20 });
+    // console.log(response.result.trades);
+    console.log(response.result.trades, 'hi paul');
+
+    // console.log(response.result.trades.length,'length');
     return [];
   }
 };
